c5646b2754258a40f4b98d47131dd09a9f940b67
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-06-27 13:40:36 +1000

    conf: Moved asset files for testing purposes

---
+++ resources/levels/test.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="test" tilewidth="256" tileheight="256" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image source="../../assets/sprite_sheet/sprite_sheet.png" width="512" height="512"/>
+ </tile>
+</tileset>
