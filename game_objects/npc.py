188b092e071c29b2c3ac8286bf6f272a66973eea
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-07-12 15:47:23 +1000

    feat: NPC are now (in-place) animated

--- game_objects/npc.py
+++ game_objects/npc.py
@@ -12,7 +12,44 @@ class NPC(GameObject):
         self.player = self.eng.player
         self.npc_id = tex_id
 
+        self.scale = cfg.NPC_SETTINGS[self.npc_id]['scale']
+        self.speed = cfg.NPC_SETTINGS[self.npc_id]['speed']
+        self.size = cfg.NPC_SETTINGS[self.npc_id]['size']
+        self.attack_dist = cfg.NPC_SETTINGS[self.npc_id]['attack_dist']
+        self.health = cfg.NPC_SETTINGS[self.npc_id]['health']
+        self.damage = cfg.NPC_SETTINGS[self.npc_id]['damage']
+        self.hit_probability = cfg.NPC_SETTINGS[self.npc_id]['hit_probability']
+        self.drop_item = cfg.NPC_SETTINGS[self.npc_id]['drop_item']
+
+        self.anim_periods = cfg.NPC_SETTINGS[self.npc_id]['anim_periods']
+        self.anim_counter = 0
+        self.frame = 0
+        self.is_animated = True
+
+        # states: walk, attack, hurt, death
+        self.num_frames = None
+        self.state_tex_id = None
+        self.set_state(state='walk')
+
         self.m_model = self.get_model_matrix()
 
     def update(self):
-        pass
+        self.animate()
+
+        # set current texture
+        self.tex_id = self.state_tex_id + self.frame
+
+    def set_state(self, state='walk'):
+        self.num_frames = cfg.NPC_SETTINGS[self.npc_id]['num_frames'][state]
+        self.state_tex_id = cfg.NPC_SETTINGS[self.npc_id]['state_tex_id'][state]
+        self.frame %= self.num_frames
+
+    def animate(self):
+        if not (self.is_animated and self.app.anim_trigger):
+            return None
+
+        self.anim_counter += 1
+
+        if self.anim_counter == self.anim_periods:
+            self.anim_counter = 0
+            self.frame = (self.frame + 1) % self.num_frames
